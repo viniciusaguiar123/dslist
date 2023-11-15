267822de7ffd2a541844dcd82fef3abd16ab49a3
Vinicius <vini1068@gmail.com>
2023-11-14 22:09:31 -0300

    Consult SQL, projection, find games by list

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -2,7 +2,12 @@ package com.devsuperior.dslist.repositories;
 
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+
 }
